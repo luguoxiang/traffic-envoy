288b4bc2d54585186860660239affae22bef20a7
Guoxiang Lu <guoxiang.lu@oracle.com>
2019-11-01 11:50:15 +0800

    add inbound rule skip

--- iptable_init.sh
+++ iptable_init.sh
@@ -6,6 +6,7 @@ iptables -t nat -N ENVOY_IN_REDIRECT
 iptables -t nat -N ENVOY_OUTPUT
 iptables -t nat -N ENVOY_REDIRECT
 
+if [ -z "${DISABLE_INBOUNT_IPTABLE}"]; then 
 # Jump to the ENVOY_INBOUND chain from PREROUTING chain for all tcp traffic.
 iptables -t nat -A PREROUTING -p tcp -j ENVOY_INBOUND
 
@@ -14,13 +15,12 @@ iptables -t nat -A ENVOY_IN_REDIRECT -p tcp -j REDIRECT --to-port ${PROXY_PORT}
 
 # Avoid infinite loops. Don't redirect admin traffic directly back to Envoy
 iptables -t nat -A ENVOY_INBOUND -p tcp --dport ${PROXY_MANAGE_PORT} -j RETURN
-for port in ${INBOUND_SKIP_PORTS}; do
-	iptables -t nat -A ENVOY_INBOUND -p tcp --dport ${port} -j RETURN
-done
 
 # Redirect remaining outbound traffic to Envoy
 iptables -t nat -A ENVOY_INBOUND -p tcp -j ENVOY_IN_REDIRECT
+fi
 
+if [ -z "${DISABLE_OUTBOUNT_IPTABLE}"]; then 
 # Create a new chain for redirecting outbound traffic to the Envoy port.
 iptables -t nat -A ENVOY_REDIRECT -p tcp -j REDIRECT --to-port ${PROXY_PORT}
 
@@ -30,9 +30,6 @@ iptables -t nat -A OUTPUT -p tcp -j ENVOY_OUTPUT
 # Avoid infinite loops. Don't redirect Envoy traffic directly back to
 # Envoy for non-loopback traffic.
 iptables -t nat -A ENVOY_OUTPUT -p tcp --dport ${PROXY_MANAGE_PORT} -j RETURN
-for port in ${OUTBOUND_SKIP_PORTS}; do
-	iptables -t nat -A ENVOY_OUTPUT -p tcp --dport ${port} -j RETURN
-done
 iptables -t nat -A ENVOY_OUTPUT -m owner --uid-owner ${PROXY_UID} -j RETURN
 iptables -t nat -A ENVOY_OUTPUT -m owner --gid-owner ${PROXY_UID} -j RETURN
 
@@ -43,4 +40,5 @@ iptables -t nat -A ENVOY_OUTPUT -j ENVOY_REDIRECT
 # container-to-container traffic both of which explicitly use
 # localhost.
 iptables -t nat -A ENVOY_OUTPUT -d 127.0.0.1/32 -j RETURN
+fi
 iptables -L -t nat 
